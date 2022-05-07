91b01e92e8dce356a9c8076e3acd836f8dca70c1
AlahiraJ <alahirajeffrey@gmail.com>
2022-05-07 22:24:42 +0100

    script to verify tokens

---
+++ utils/verify_token.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken')
+const usersDB = require("../models/user_model")
+
+const verifyToken = (req, res, next) => {
+
+    const authHeader = req.headers.token
+    // return 404 error is token is not valid
+    if (!authHeader) return res.status(401).json({ message: "Not authenticated" })
+
+    // retrieve token if auth header is present
+    const token = authHeader.split(" ")[1]
+    jwt.verify(token, process.env.JWT_TOKEN_KEY, (err, result) => {
+        if (err) return res.status(403).json("Invalid token")
+        req.user = user
+        next()
+    })
+}
+
+module.exports = verifyToken 
