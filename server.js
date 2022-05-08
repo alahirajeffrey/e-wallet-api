41e896289bd02653615610661de218d26a43d72e
AlahiraJ <alahirajeffrey@gmail.com>
2022-05-08 08:12:05 +0100

    added index route and user route

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require("express")
 const server = express()
 
 const authRoutes = require("./routes/auth_routes")
+const userRoutes = require("./routes/user_routes")
 
 server.use(express.json())
 server.use("/api/v1/auth", authRoutes)
-//server.use("/api/v1/wallet", wallet_routes)
+server.use("/api/v1/user", userRoutes)
+
+server.get("/", (req, res) => {
+    return res.status(200).json({ message: "Welcome!" })
+});
 
 module.exports = server
