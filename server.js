5b488796bd07326ff7d0c64f96d01c29cb81c590
AlahiraJ <alahirajeffrey@gmail.com>
2022-05-09 11:33:22 +0100

    added wallet routes

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const server = express()
 
 const authRoutes = require("./routes/auth_routes")
 const userRoutes = require("./routes/user_routes")
+const walletRoutes = require("./routes/wallet_routes")
 
 server.use(express.json())
 server.use("/api/v1/auth", authRoutes)
 server.use("/api/v1/user", userRoutes)
+server.use("/api/v1/wallet", walletRoutes)
 
 server.get("/", (req, res) => {
     return res.status(200).json({ message: "Welcome!" })
