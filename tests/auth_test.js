fd69e8d837382aa691a821bfc116f36f1f260998
AlahiraJ <alahirajeffrey@gmail.com>
2022-05-11 15:23:53 +0100

    test

--- tests/auth_test.js
+++ tests/auth_test.js
@@ -1,169 +1,169 @@
-// const chai = require("chai")
-// const chaiHttp = require("chai-http")
-// const server = require("../server")
+const chai = require("chai")
+const chaiHttp = require("chai-http")
+const server = require("../server")
 
-// const should = chai.should()
+const should = chai.should()
 
-// chai.use(chaiHttp)
+chai.use(chaiHttp)
 
-// /**
-//  * TEST the /POST route
-//  */
-// describe('User registration', () => {
+/**
+ * TEST the /POST route
+ */
+describe('User registration', () => {
 
-//     it('should return a 400 error when email is absent ', (done) => {
+    it('should return a 400 error when email is absent ', (done) => {
 
-//         const user = {
-//             firstName: "Jeffrey",
-//             lastName: "Alahira",
-//             password: '123456',
-//         }
+        const user = {
+            firstName: "Jeffrey",
+            lastName: "Alahira",
+            password: '123456',
+        }
 
-//         chai.request(server)
-//             .post('/api/v1/auth/register')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(400)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message')
-//                 done()
-//             })
-//     })
+        chai.request(server)
+            .post('/api/v1/auth/register')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(400)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message')
+                done()
+            })
+    })
 
-//     it('Should register a new user and create a wallet', (done) => {
+    it('Should register a new user and create a wallet', (done) => {
 
-//         const user = {
-//             firstName: "Jeffrey",
-//             lastName: "Alahira",
-//             email: 'alahirajeffrey@gmail.com',
-//             password: '123456',
-//             mobileNumber: "0701234"
-//         }
+        const user = {
+            firstName: "Jeffrey",
+            lastName: "Alahira",
+            email: 'alahirajeffrey@gmail.com',
+            password: '123456',
+            mobileNumber: "0701234"
+        }
 
-//         chai.request(server)
-//             .post('/api/v1/auth/register')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(201)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message').eq("New user registered and wallet created")
-//                 done()
-//             })
-//     })
+        chai.request(server)
+            .post('/api/v1/auth/register')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(201)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message').eq("New user registered and wallet created")
+                done()
+            })
+    })
 
-//     it('should return a 409 error if user with similar email already exists', (done) => {
+    it('should return a 409 error if user with similar email already exists', (done) => {
 
-//         const user = {
-//             firstName: "Jeffrey",
-//             lastName: "Alahira",
-//             email: 'alahirajeffrey@gmail.com',
-//             password: '123456',
-//             mobileNumber: "0701234"
-//         }
+        const user = {
+            firstName: "Jeffrey",
+            lastName: "Alahira",
+            email: 'alahirajeffrey@gmail.com',
+            password: '123456',
+            mobileNumber: "0701234"
+        }
 
-//         chai.request(server)
-//             .post('/api/v1/auth/register')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(409)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message')
-//                 done()
-//             })
-//     })
-// })
-
-
-// describe('User login', () => {
-
-//     it('should return a 400 error when email is not entered', (done) => {
-
-//         const user = { password: '123456' }
-
-//         chai.request(server)
-//             .get('/api/v1/auth/login')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(400)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message')
-//                 done()
-//             })
-//     })
-
-//     it('should return a 500 error when wrong email is entered', (done) => {
-
-//         const user = {
-//             email: 'esther@gmail.com',
-//             password: '123456'
-//         }
-
-//         chai.request(server)
-//             .get('/api/v1/auth/login')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(500)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message')
-//                 done()
-//             })
-//     })
-
-//     it('should return a 400 error when incorrect password is entered', (done) => {
-
-//         const user = {
-//             email: 'alahirajeffrey@gmail.com',
-//             password: '0000'
-//         }
-
-//         chai.request(server)
-//             .get('/api/v1/auth/login')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(401)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message')
-//                 done()
-//             })
-//     })
-
-//     it('Should login user', (done) => {
-
-//         const user = {
-//             email: 'alahirajeffrey@gmail.com',
-//             password: '123456'
-//         }
-
-//         chai.request(server)
-//             .get('/api/v1/auth/login')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(200)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message')
-//                 res.body.should.have.property('token')
-//                 // get token from request body to test user routes
-//                 const token = res.body.token
+        chai.request(server)
+            .post('/api/v1/auth/register')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(409)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message')
+                done()
+            })
+    })
+})
 
 
-//                 describe('DELETE user', () => {
-//                     it('should delete user', (done) => {
+describe('User login', () => {
 
-//                         const mobileNumber = "0701234"
+    it('should return a 400 error when email is not entered', (done) => {
 
-//                         chai.request(server)
-//                             .delete('/api/v1/user/' + mobileNumber)
-//                             .send(user)
-//                             .set('token', 'Bearer ' + token)
-//                             .end((err, response) => {
-//                                 response.should.have.status(204)
-//                                 response.should.be.a('object')
+        const user = { password: '123456' }
 
-//                                 done()
-//                             })
-//                     })
-//                 })
-//                 done()
-//             })
-//     })
-// })
+        chai.request(server)
+            .get('/api/v1/auth/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(400)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message')
+                done()
+            })
+    })
+
+    it('should return a 500 error when wrong email is entered', (done) => {
+
+        const user = {
+            email: 'esther@gmail.com',
+            password: '123456'
+        }
+
+        chai.request(server)
+            .get('/api/v1/auth/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(500)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message')
+                done()
+            })
+    })
+
+    it('should return a 400 error when incorrect password is entered', (done) => {
+
+        const user = {
+            email: 'alahirajeffrey@gmail.com',
+            password: '0000'
+        }
+
+        chai.request(server)
+            .get('/api/v1/auth/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(401)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message')
+                done()
+            })
+    })
+
+    it('Should login user', (done) => {
+
+        const user = {
+            email: 'alahirajeffrey@gmail.com',
+            password: '123456'
+        }
+
+        chai.request(server)
+            .get('/api/v1/auth/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message')
+                res.body.should.have.property('token')
+                // get token from request body to test user routes
+                const token = res.body.token
+
+
+                describe('DELETE user', () => {
+                    it('should delete user', (done) => {
+
+                        const mobileNumber = "0701234"
+
+                        chai.request(server)
+                            .delete('/api/v1/user/' + mobileNumber)
+                            .send(user)
+                            .set('token', 'Bearer ' + token)
+                            .end((err, response) => {
+                                response.should.have.status(204)
+                                response.should.be.a('object')
+
+                                done()
+                            })
+                    })
+                })
+                done()
+            })
+    })
+})
--- tests/user_test.js
+++ tests/user_test.js
@@ -1,144 +1,144 @@
-// const chai = require("chai")
-// const chaiHttp = require("chai-http")
-// const server = require("../server")
+const chai = require("chai")
+const chaiHttp = require("chai-http")
+const server = require("../server")
 
-// const should = chai.should()
+const should = chai.should()
 
-// chai.use(chaiHttp)
+chai.use(chaiHttp)
 
-// describe('Register user to test user routes', () => {
+describe('Register user to test user routes', () => {
 
-//     it('Should register a new user and create a wallet', (done) => {
+    it('Should register a new user and create a wallet', (done) => {
 
-//         const user = {
-//             firstName: "daniel",
-//             lastName: "alahira",
-//             email: 'danielalahira@gmail.com',
-//             password: '123456',
-//             mobileNumber: "070123456"
-//         }
+        const user = {
+            firstName: "daniel",
+            lastName: "alahira",
+            email: 'danielalahira@gmail.com',
+            password: '123456',
+            mobileNumber: "070123456"
+        }
 
-//         chai.request(server)
-//             .post('/api/v1/auth/register')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(201)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message').eq("New user registered and wallet created")
-//                 done()
-//             })
-//     })
+        chai.request(server)
+            .post('/api/v1/auth/register')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(201)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message').eq("New user registered and wallet created")
+                done()
+            })
+    })
 
-//     it('Should login user', (done) => {
+    it('Should login user', (done) => {
 
-//         const user = {
-//             email: 'danielalahira@gmail.com',
-//             password: '123456'
-//         }
+        const user = {
+            email: 'danielalahira@gmail.com',
+            password: '123456'
+        }
 
-//         chai.request(server)
-//             .get('/api/v1/auth/login')
-//             .send(user)
-//             .end((err, res) => {
-//                 res.should.have.status(200)
-//                 res.body.should.be.a('object')
-//                 res.body.should.have.property('message')
-//                 res.body.should.have.property('token')
-//                 // get token from request body to test user routes
-//                 const token = res.body.token
-//                 done()
+        chai.request(server)
+            .get('/api/v1/auth/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a('object')
+                res.body.should.have.property('message')
+                res.body.should.have.property('token')
+                // get token from request body to test user routes
+                const token = res.body.token
+                done()
 
-//                 /**
-//                 * Test PUT route
-//                 */
+                /**
+                * Test PUT route
+                */
 
-//                 describe("User routes test", () => {
+                describe("User routes test", () => {
 
-//                     it('should return a 200 code when user is updated', (done) => {
+                    it('should return a 200 code when user is updated', (done) => {
 
-//                         const mobileNumber = "070123456"
+                        const mobileNumber = "070123456"
 
-//                         const user = {
-//                             firstName: "alahira",
-//                             lastName: "daniel",
-//                             email: "danielalahira@gmail.com"
-//                         }
+                        const user = {
+                            firstName: "alahira",
+                            lastName: "daniel",
+                            email: "danielalahira@gmail.com"
+                        }
 
-//                         chai.request(server)
-//                             .put('/api/v1/user/' + mobileNumber)
-//                             .send(user)
-//                             .set('token', 'Bearer ' + token)
-//                             .end((err, response) => {
-//                                 response.should.have.status(200)
-//                                 response.should.be.a('object')
-//                                 response.body.should.have.property('message').eq("User updated...")
-//                                 done()
-//                             })
-//                     })
+                        chai.request(server)
+                            .put('/api/v1/user/' + mobileNumber)
+                            .send(user)
+                            .set('token', 'Bearer ' + token)
+                            .end((err, response) => {
+                                response.should.have.status(200)
+                                response.should.be.a('object')
+                                response.body.should.have.property('message').eq("User updated...")
+                                done()
+                            })
+                    })
 
-//                     it('should return a 400 error when request is not given', (done) => {
+                    it('should return a 400 error when request is not given', (done) => {
 
-//                         const mobileNumber = "070123456"
+                        const mobileNumber = "070123456"
 
-//                         chai.request(server)
-//                             .put('/api/v1/user/' + mobileNumber)
-//                             .send()
-//                             .set('token', 'Bearer ' + token)
-//                             .end((err, response) => {
-//                                 response.should.have.status(400)
-//                                 response.should.be.a('object')
-//                                 response.body.should.have.property('message').eq("Error updating. Include firstName, lastName or email...")
-//                                 done()
-//                             })
-//                     })
-//                 })
+                        chai.request(server)
+                            .put('/api/v1/user/' + mobileNumber)
+                            .send()
+                            .set('token', 'Bearer ' + token)
+                            .end((err, response) => {
+                                response.should.have.status(400)
+                                response.should.be.a('object')
+                                response.body.should.have.property('message').eq("Error updating. Include firstName, lastName or email...")
+                                done()
+                            })
+                    })
+                })
 
-//                 /**
-//                 * Test DELETE route
-//                 *
-//                 */
+                /**
+                * Test DELETE route
+                *
+                */
 
-//                 describe('DELETE user', () => {
-//                     it('should return a 400 error if email is not given', (done) => {
+                describe('DELETE user', () => {
+                    it('should return a 400 error if email is not given', (done) => {
 
-//                         const mobileNumber = "070123456"
+                        const mobileNumber = "070123456"
 
-//                         const user = {}
+                        const user = {}
 
-//                         chai.request(server)
-//                             .delete('/api/v1/user/' + mobileNumber)
-//                             .send(user)
-//                             .set('token', 'Bearer ' + token)
-//                             .end((err, response) => {
-//                                 response.should.have.status(400)
-//                                 response.should.be.a('object')
-//                                 done()
-//                             })
-//                     })
-//                 })
+                        chai.request(server)
+                            .delete('/api/v1/user/' + mobileNumber)
+                            .send(user)
+                            .set('token', 'Bearer ' + token)
+                            .end((err, response) => {
+                                response.should.have.status(400)
+                                response.should.be.a('object')
+                                done()
+                            })
+                    })
+                })
 
-//                 describe('DELETE user', () => {
-//                     it('should return 200 error if user is deleted', (done) => {
+                describe('DELETE user', () => {
+                    it('should return 200 error if user is deleted', (done) => {
 
-//                         const mobileNumber = "070123456"
+                        const mobileNumber = "070123456"
 
-//                         const user = {
-//                             email: "danielalahira@gmail.com"
-//                         }
+                        const user = {
+                            email: "danielalahira@gmail.com"
+                        }
 
-//                         chai.request(server)
-//                             .delete('/api/v1/user/' + mobileNumber)
-//                             .send(user)
-//                             .set('token', 'Bearer ' + token)
-//                             .end((err, response) => {
-//                                 response.should.have.status(204)
-//                                 response.should.be.a('object')
-//                                 done()
-//                             })
-//                     })
-//                 })
+                        chai.request(server)
+                            .delete('/api/v1/user/' + mobileNumber)
+                            .send(user)
+                            .set('token', 'Bearer ' + token)
+                            .end((err, response) => {
+                                response.should.have.status(204)
+                                response.should.be.a('object')
+                                done()
+                            })
+                    })
+                })
 
 
-//             })
-//     })
-// })
+            })
+    })
+})
